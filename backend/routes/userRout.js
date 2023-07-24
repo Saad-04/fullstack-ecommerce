28f7ed2fa3d709ca77d69b83ea129ea7f4d7fdfb
Saad-04 <asaad4674@gmail.com>
2023-07-24 16:27:16 -0700

    twelve

--- backend/routes/userRout.js
+++ backend/routes/userRout.js
@@ -2,9 +2,9 @@ const express = require('express')
 const {registerUser, loginUser, logoutUser, forgotPassword} = require('../controllers/userController.js');
 const router = express.Router();
 
-router.route('/register').post(registerUser)
-router.route('/login').post(loginUser)
-router.route('/password/forgot').post(forgotPassword)
-router.route('/logout').get(logoutUser)
+router.route('/register').post(registerUser);
+router.route('/login').post(loginUser);
+router.route('/password/forgot').post(forgotPassword);
+router.route('/logout').get(logoutUser);
 
 module.exports = router;
