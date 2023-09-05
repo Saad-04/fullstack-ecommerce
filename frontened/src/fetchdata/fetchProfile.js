445b645196af2a09c5dcebef2064acd3e95ae3e7
Saad Ali <115668468+Saad-04@users.noreply.github.com>
2023-09-05 23:58:05 +0500

    Update fetchProfile.js

--- frontened/src/fetchdata/fetchProfile.js
+++ frontened/src/fetchdata/fetchProfile.js
@@ -5,7 +5,7 @@ export const userProfile = createAsyncThunk("user/profile", async (thunkAPI) => 
     try {
         const { data } = await axios.get(
             `/api/v1/profile`);
-        return await data.user
+        return await data.user;
     } catch (error) {
         return error.response.data.message
     }
